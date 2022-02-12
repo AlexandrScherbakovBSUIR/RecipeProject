3572b3bf52330ec917f8731769405bf35f65516b
alexa <alexandrscherbakov231194@gmail.com>
2022-02-12 20:10:43 +0300

    add todo and add ArgumentCaptor

--- src/test/java/guru/springframework/recipeproject/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/recipeproject/controllers/RecipeControllerTest.java
@@ -1,21 +1,26 @@
 package guru.springframework.recipeproject.controllers;
 
+import guru.springframework.recipeproject.domain.Recipe;
 import guru.springframework.recipeproject.repositories.RecipeRepository;
 import guru.springframework.recipeproject.services.RecipeService;
 import guru.springframework.recipeproject.services.RecipeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class RecipeControllerTest {
     //TODO: initialisation and how mocks work
+    //TODO: ArgumentCaptor, when(), verify()
     @Mock
     Model model;
     @Mock
@@ -33,12 +38,23 @@ class RecipeControllerTest {
      //   recipeService = new RecipeServiceImpl(recipeRepository);
         System.out.println(recipeService.toString());
      //   model.addAttribute("recipes",recipeService.findAllRecipes());
+
     }
 
     @Test
     void getListOfRecipes() {
+        List<Recipe> listOfRecipes = new ArrayList<Recipe>();
+        Recipe recipe1 = new Recipe();
+        Recipe recipe2 = new Recipe();
+        recipe1.setId(1l);
+        recipe2.setId(2l);
+
+        listOfRecipes.add(recipe1);
+        listOfRecipes.add(recipe2);
+        ArgumentCaptor<List<Recipe>> listArgumentCaptor = ArgumentCaptor.forClass(List.class);
+        when(recipeService.findAllRecipes()).thenReturn(listOfRecipes);
         assertEquals(recipeController.getListOfRecipes(model),"recipes/listOfRecipes");
         verify(recipeService,times(1)).findAllRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"),anyList());
+        verify(model,times(1)).addAttribute(eq("recipes"),listArgumentCaptor.capture());
     }
 }
