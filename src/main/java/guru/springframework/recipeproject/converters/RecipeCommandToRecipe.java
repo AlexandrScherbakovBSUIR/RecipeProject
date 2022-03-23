6afd7b4152027f81910d572b91d129d1c915c03e
Alexandr Scherbakov <alexandrscherbakov231194@gmail.com>
2022-03-23 12:14:41 +0100

    implementing REcipe's converters

--- src/main/java/guru/springframework/recipeproject/converters/RecipeCommandToRecipe.java
+++ src/main/java/guru/springframework/recipeproject/converters/RecipeCommandToRecipe.java
@@ -1,4 +1,59 @@
 package guru.springframework.recipeproject.converters;
 
-public class RecipeCommandToRecipe {
+import guru.springframework.recipeproject.commands.CategoryCommand;
+import guru.springframework.recipeproject.commands.IngredientCommand;
+import guru.springframework.recipeproject.commands.RecipeCommand;
+import guru.springframework.recipeproject.domain.Recipe;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecipeCommandToRecipe implements Converter<RecipeCommand,Recipe> {
+    private final IngredientCommandToIngredient ingredientCommandToIngredient;
+    private final CategoryCommandToCategory categoryCommandToCategory;
+    private final RecipeNoteCommandToRecipeNote recipeNoteCommandToRecipeNote;
+
+    public RecipeCommandToRecipe(IngredientCommandToIngredient ingredientCommandToIngredient,
+                                 CategoryCommandToCategory categoryCommandToCategory,
+                                 RecipeNoteCommandToRecipeNote recipeNoteCommandToRecipeNote) {
+        this.ingredientCommandToIngredient = ingredientCommandToIngredient;
+        this.categoryCommandToCategory = categoryCommandToCategory;
+        this.recipeNoteCommandToRecipeNote = recipeNoteCommandToRecipeNote;
+    }
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Recipe convert(RecipeCommand source) {
+        if(source!=null){
+            Recipe recipe = new Recipe();
+
+            recipe.setId(source.getId());
+            recipe.setCookTime(source.getCookTime());
+            recipe.setPrepTime(source.getPrepTime());
+            recipe.setServing(source.getServing());
+            recipe.setDescription(source.getDescription());
+            recipe.setDirections(source.getDirections());
+            recipe.setDifficulty(source.getDifficulty());
+            recipe.setSource(source.getSource());
+            recipe.setUrl(source.getUrl());
+            recipe.setRecipeNote(recipeNoteCommandToRecipeNote.convert(source.getRecipeNote()));
+
+            if(source.getCategories()!=null && !source.getCategories().isEmpty()){
+                for (CategoryCommand categoryCommand:source.getCategories()) {
+                    recipe.getCategories().add(categoryCommandToCategory.convert(categoryCommand));
+                }
+            }
+
+            if(source.getIngredients()!=null && !source.getIngredients().isEmpty()){
+                for (IngredientCommand ingredientCommand: source.getIngredients()) {
+                    recipe.getIngredients().add(ingredientCommandToIngredient.convert(ingredientCommand));
+                                    }
+            }
+
+        }
+        return null;
+    }
 }
--- src/main/java/guru/springframework/recipeproject/converters/RecipeToRecipeCommand.java
+++ src/main/java/guru/springframework/recipeproject/converters/RecipeToRecipeCommand.java
@@ -1,20 +1,59 @@
 package guru.springframework.recipeproject.converters;
 
 import guru.springframework.recipeproject.commands.RecipeCommand;
+import guru.springframework.recipeproject.domain.Category;
+import guru.springframework.recipeproject.domain.Ingredient;
 import guru.springframework.recipeproject.domain.Recipe;
 import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RecipeToRecipeCommand implements Converter<Recipe, RecipeCommand> {
+    private final IngredientToIngredientCommand ingredientToIngredientCommand ;
+    private final CategoryToCategoryCommand categoryToCategoryCommand;
+    private final RecipeNoteToRecipeNoteCommand recipeNoteToRecipeNoteCommand;
+
+    public RecipeToRecipeCommand(IngredientToIngredientCommand ingredientToIngredientCommand,
+                                 CategoryToCategoryCommand categoryToCategoryCommand,
+                                 RecipeNoteToRecipeNoteCommand recipeNoteToRecipeNoteCommand) {
+        this.ingredientToIngredientCommand = ingredientToIngredientCommand;
+        this.categoryToCategoryCommand = categoryToCategoryCommand;
+        this.recipeNoteToRecipeNoteCommand = recipeNoteToRecipeNoteCommand;
+    }
 
     @Synchronized
     @Nullable
     @Override
     public RecipeCommand convert(Recipe source) {
         if(source!=null){
+            RecipeCommand recipeCommand = new RecipeCommand();
 
+            recipeCommand.setId(source.getId());
+            recipeCommand.setPrepTime(source.getPrepTime());
+            recipeCommand.setCookTime(source.getCookTime());
+            recipeCommand.setServing(source.getServing());
+            recipeCommand.setDescription(source.getDescription());
+            recipeCommand.setDirections(source.getDirections());
+            recipeCommand.setDifficulty(source.getDifficulty());
+            recipeCommand.setUrl(source.getUrl());
+            recipeCommand.setSource(source.getSource());
+            recipeCommand.setRecipeNote(recipeNoteToRecipeNoteCommand.convert(source.getRecipeNote()));
 
+            if(source.getCategories()!=null && !source.getCategories().isEmpty()){
+                for (Category category: source.getCategories() ) {
+                    recipeCommand.getCategories().add(categoryToCategoryCommand.convert(category));
+
+                }
+            }
+
+            if(source.getIngredients()!=null && !source.getIngredients().isEmpty()){
+                for (Ingredient ingredient: source.getIngredients()) {
+                    recipeCommand.getIngredients().add(ingredientToIngredientCommand.convert(ingredient));
+                }
+            }
+            return recipeCommand;
         }
 
         return null;
